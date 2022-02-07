6ddcbf306ceb3de0097dff73d6c6b42d138ca968
David Joaquín Shourabi Porcel <david@djsp.eu>
2022-02-07 01:18:15 +0100

    Add startup script for Bash interactive non-login shells

---
+++ bashrc.sh
@@ -0,0 +1,9 @@
+# info:(bash)Bash_Startup_Files
+# man:bash(1)
+
+# This script is read and executed by GNU Bash when
+#
+# - Bash is invoked as an interactive non-login shell or
+# - Bash is invoked by a remote shell daemon, like `sshd`,
+#
+# unless Bash is given the option `--norc`.
