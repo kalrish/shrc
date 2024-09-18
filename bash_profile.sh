68d5593f0cf76e53ee26c063c1a9f494b0e53d2a
David Joaquín Shourabi Porcel <david@djsp.eu>
2024-09-18 12:20:36 +0200

    Source `~/.profile` from `~/.bash_profile`

--- bash_profile.sh
+++ bash_profile.sh
@@ -2,3 +2,11 @@
 # man:bash(1)
 
 # This script is sourced by Bash when invoked as a login shell.
+
+
+# Bash ignores `~/.profile` if it finds `~/.bash_profile`.
+# By loading `~/.profile` manually, initialization for all sh-derived shells may be kept in `~/.profile`.
+if [[ -f ~/.profile ]]
+then
+	source -- ~/.profile
+fi
