ab7ce5f6782ac18b4254878eae48735337450eaa
David Joaquín Shourabi Porcel <david@djsp.eu>
2024-09-18 12:39:03 +0200

    Have sh-derived login shells set locale environment variables from `${XDG_CONFIG_HOME}/locale.conf` on startup

--- profile.sh
+++ profile.sh
@@ -1,1 +1,28 @@
 # This script is sourced by sh-derived shells like Bash invoked as a login shell.
+
+
+# User locale preferences are best set as environment variables in [JSON user records](https://systemd.io/USER_RECORD).
+# A different solution is required for users defined in `/etc/passwd`.
+# Arch Linux has sh-derived login shells source `${XDG_CONFIG_HOME}/locale.conf` and export the locale variables set therein.
+# The code below extends that mechanism to other distros.
+if test -f "${XDG_CONFIG_HOME:-${HOME}/.config}/locale.conf"
+then
+	. -- "${XDG_CONFIG_HOME:-${HOME}/.config}/locale.conf"
+	export \
+		-- \
+		LANG \
+		LANGUAGE \
+		LC_ADDRESS \
+		LC_COLLATE \
+		LC_CTYPE \
+		LC_IDENTIFICATION \
+		LC_MEASUREMENT \
+		LC_MESSAGES \
+		LC_MONETARY \
+		LC_NAME \
+		LC_NUMERIC \
+		LC_PAPER \
+		LC_TELEPHONE \
+		LC_TIME \
+		#
+fi
